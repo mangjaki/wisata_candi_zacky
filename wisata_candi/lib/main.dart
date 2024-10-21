173947aea2fcbd75d03cd3fe816bea8d58526716
mangjaki <cungcang996@gmail.com>
2024-10-21 13:04:54 +0700

    update

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
 
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi: candiList[0]),
     );
   }
 }
